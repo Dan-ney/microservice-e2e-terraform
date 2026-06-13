886faf478b2a8938a57bb97aaba8ab0bc0ffd3bc
Dan-ney <daniel.paul@enhub.ai>
2026-06-13 17:09:48 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,14 +1,9 @@
-terraform {
-  required_version = ">= 1.0.0"
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
 
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 5.0"
-    }
-  }
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
 }
