44f7eb91d47b458d9634fb080e1e297cb253dd10
Dan-ney <daniel.paul@enhub.ai>
2026-06-13 19:27:40 +0530

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  project_id    = var.project_id
+  region        = var.region
+  environment   = var.environment
+  vpc_cidr      = "10.0.0.0/16"
+  pods_cidr     = "10.1.0.0/16"
+  services_cidr = "10.2.0.0/16"
+
+  depends_on = [google_project_service.required_apis]
+}
