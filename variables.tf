b4f0a7f5763ff8706a21b40527393c9072cb1f50
Dan-ney <daniel.paul@enhub.ai>
2026-06-14 18:37:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,3 +20,13 @@ variable "environment" {
   type        = string
   default     = "prod"
 }
+
+variable "argocd_workload_identity_name" {
+  type        = string
+  description = "The name of the Google Cloud Service Account created for ArgoCD"
+}
+
+variable "argocd_repo_sa_name" {
+  type        = string
+  description = "The name of the internal Kubernetes Service Account used by the argo repo server"
+}
