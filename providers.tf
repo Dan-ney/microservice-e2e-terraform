a52ba98c28c7bd6032a4530f476abaa09d03b730
Dan-ney <daniel.paul@enhub.ai>
2026-06-14 18:45:35 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,7 +13,7 @@ data "google_client_config" "default" {}
 data "google_container_cluster" "primary" {
   name     = "${var.environment}-gke-cluster"
   location = var.zone
-  project  = var.project_ID
+  project  = var.project_id
 }
 
 provider "helm" {
